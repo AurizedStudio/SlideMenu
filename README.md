a776cd48b4edbb3ea635744f9dc37895385e1aba
T.Suzaki <info@aurized-studio.jp>
2015-01-09 11:01:36 +0900

    1st commite

---
+++ README.md
@@ -0,0 +1,4 @@
+# Slide Menu
+
+I'm trying to make global menu like Apple mobile site style.  
+Using "Owl Carousel 2".
---
+++ htdocs/js/style-top.js
@@ -0,0 +1,39 @@
+
+$(function(){
+	var owl = $('.owl-carousel');
+	owl.owlCarousel({
+		dots: false,
+//		mouseDrag: false,
+		startPosition: 2,
+		responsive: {
+			0: {
+				items: 5
+			},
+			375: {
+				items: 6
+			},
+			568: {
+				items: 7
+			},
+			667: {
+				items: 8
+			}
+//			736: {
+//				items: 8
+//			}
+		},
+		onInitialized: initial, // When the plugin has initialized.
+	})
+	function initial(){
+		owl.trigger('prev.owl.carousel', [600])
+		owl.trigger('prev.owl.carousel', [600])
+	}
+});
+
+
+// グロナビの高さを揃える
+$(function(){
+	$(".gnav a").coladjust({
+		adjust: false
+	});
+});
